df0199147a19af7190a1a289cad379beaf396bfd
Skipcast <jonathanjl99@gmail.com>
2020-08-01 13:53:55 +0200

    Removed MomentSharp and implemented custom simpler version of the small functionality that was needed

--- Ets2SyncWindows/DateToTimeAgoConverter.cs
+++ Ets2SyncWindows/DateToTimeAgoConverter.cs
@@ -3,8 +3,6 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Data;
-using MomentSharp;
-using MomentSharp.Globalization.Languages;
 
 namespace Ets2SyncWindows
 {
@@ -15,7 +13,29 @@ namespace Ets2SyncWindows
             if (!(value is DateTime dateTime))
                 throw new ArgumentException("Specified value is not a DateTime.", nameof(value));
 
-            string result = dateTime.Moment().From();
+            string result;
+            TimeSpan timeSince = DateTime.UtcNow - dateTime.ToUniversalTime();
+
+            if (timeSince.TotalSeconds < 60)
+            {
+                result = $"{timeSince.TotalSeconds:0} second{((int) timeSince.TotalSeconds != 1 ? "s" : "")} ago";
+            }
+            else if (timeSince.TotalMinutes < 60)
+            {
+                result = $"{timeSince.TotalMinutes:0} minute{((int) timeSince.TotalMinutes != 1 ? "s" : "")} ago";
+            }
+            else if (timeSince.TotalHours < 24)
+            {
+                result = $"{timeSince.TotalHours:0} hour{((int) timeSince.TotalHours != 1 ? "s" : "")} ago";
+            }
+            else if (timeSince.TotalDays < 7)
+            {
+                result = $"{timeSince.TotalDays:0} day{((int) timeSince.TotalDays != 1 ? "s" : "")} ago";
+            }
+            else
+            {
+                result = dateTime.ToShortDateString();
+            }
 
             if (parameter is string strParameter && strParameter == "initialLowerCase")
             {
